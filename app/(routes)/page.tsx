7a45c5884d0396fa5a48f4571c374db4d8f7257b
Rafal <rafalciechanski15@gmail.com>
2023-07-22 22:32:39 +0300

    Setup, product card

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,25 @@
+import getBillboard from '@/actions/get-billboard'
+import getProducts from '@/actions/get-products'
+import Billboard from '@/components/ui/billboard'
+import Container from '@/components/ui/container'
+import ProductList from '@/components/ui/product-list'
+
+
+export const revalidate = 0
+
+const HomePage = async () => {
+    const billboard = await getBillboard("ed4eacea-504d-44c0-9822-be3ee5102580")
+    const products = await getProducts({ isFeatured: true })
+    return (
+        <Container>
+            <div className='space-y-10 pb-10'>
+                <Billboard data={billboard} />
+            </div>
+            <div className='flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8'>
+                <ProductList title="Featured products" items={products} />
+            </div>
+        </Container>
+    )
+}
+
+export default HomePage
