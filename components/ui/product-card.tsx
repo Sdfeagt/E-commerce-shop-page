7bc2676259001e39694e5bc9727e4bc4d3d60386
Rafal <rafalciechanski15@gmail.com>
2023-07-23 15:46:18 +0300

    Added a product card

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -6,21 +6,25 @@ import Image from "next/image"
 import IconButton from '@/components/ui/icon-button'
 import { Expand, ShoppingCart } from 'lucide-react'
 import Currency from '@/components/ui/currency'
+import { useRouter } from 'next/navigation'
 
 interface ProductCard {
     data: Product
 }
 
 const ProductCard: React.FC<ProductCard> = ({ data }) => {
+    const router = useRouter()
+
+    const handleClick = () => {
+        router.push(`/product/${data?.id}`)
+    }
     return (
-        <div className='bg-white group cursor-pointer rounded-xl border p-3 space-y-4'>
+        <div onClick={handleClick} className='bg-white group cursor-pointer rounded-xl border p-3 space-y-4'>
             <div className='aspect-square rounded-xl bg-gray-100 relative'>
                 <Image alt="image" src={data?.images?.[0]?.url} fill className='aspect-square object-cover rounded-md' />
                 <div className='opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5'>
                     <div className='flex gap-x-6 justify-center'>
                         <IconButton onClick={() => { }} icon={<Expand size={20} className="text-gray-600" />} />
-                    </div>
-                    <div className='flex gap-x-6 justify-center'>
                         <IconButton onClick={() => { }} icon={<ShoppingCart size={20} className="text-gray-600" />} />
                     </div>
                 </div>
